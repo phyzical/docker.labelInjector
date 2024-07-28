7f08561bcf39dcde9f84f67b771ae530d58ef8de
phyzical <5182053+phyzical@users.noreply.github.com>
2024-07-28 19:05:12 +0800

    inital release

--- src/docker.labelInjector/usr/local/emhttp/plugins/docker.labelInjector/scripts/docker.js
+++ src/docker.labelInjector/usr/local/emhttp/plugins/docker.labelInjector/scripts/docker.js
@@ -1,12 +1,154 @@
 $(document).ready(function () {
-    $("#docker_containers").after('<input type="button" onclick="addLabels()" value="Add LAbels" style="">')
+    $("#docker_containers").after('<input type="button" onclick="labelFormPopup()" value="Add Labels" style="">')
 })
 
+function labelForm() {
+    const tempDiv = $('#temp-label-injector-form')
+    const form = document.createElement('form');
+    form.className = "label-injector-form"
+    const formGroup = document.createElement('div');
+
+    const containerHelp = document.createElement('p');
+    containerHelp.textContent = 'Click on the containers to add labels to';
+
+    const containerHelp2 = document.createElement('p');
+    containerHelp2.textContent = 'Click on "All" to select all containers';
+
+    const containerSelect = document.createElement('select');
+    containerSelect.name = 'containers';
+    containerSelect.multiple = true
+    containerSelect.id = 'label-injector-containers';
+
+
+    const allOption = document.createElement('option');
+    allOption.value = 'All';
+    allOption.text = 'All';
+
+    allOption.addEventListener('click', function () {
+        if (allOption.selected) {
+            for (let i = 0; i < containerSelect.options.length; i++) {
+                containerSelect.options[i].selected = allOption.selected;
+            }
+        };
+    })
+
+    containerSelect.appendChild(allOption);
+
+    docker.forEach(ct => {
+        const option = document.createElement('option');
+        option.value = ct.name;
+        option.text = ct.name;
+        containerSelect.appendChild(option);
+    })
+
+    formGroup.appendChild(containerHelp);
+    formGroup.appendChild(containerHelp2);
+    formGroup.appendChild(containerSelect);
+    form.appendChild(formGroup);
+
+
+    const labelInput = document.createElement('input');
+    labelInput.type = 'text';
+    labelInput.className = 'label-injector-input';
+    labelInput.placeholder = 'Enter label name';
+
+    const valueInput = document.createElement('input');
+    valueInput.type = 'text';
+    valueInput.className = 'label-injector-input';
+    valueInput.placeholder = 'Enter label value';
+
+    const addButton = document.createElement('button');
+    addButton.type = 'button';
+    addButton.textContent = 'Add';
+
+    const labelHelp = document.createElement('p');
+    labelHelp.textContent = 'Click on a label to remove it';
+
+    const labelSelect = document.createElement('select');
+    labelSelect.name = 'labels';
+    labelSelect.id = 'label-injector-labels';
+    labelSelect.multiple = true
+    labelSelect.disabled = true;
+
+    addButton.addEventListener('click', function () {
+        if (labelInput.value.trim() !== '' && valueInput.value.trim() !== '') {
+            const customOption = document.createElement('option');
+            customOption.value = valueInput.value;
+            customOption.text = labelInput.value;
+            customOption.selected = true;
+            customOption.addEventListener('click', function () {
+                labelSelect.removeChild(customOption);
+            })
+            labelSelect.appendChild(customOption);
+            labelInput.value = ''; // Clear the input field
+            valueInput.value = ''; // Clear the input field
+        }
+    });
+
+    const formGroup2 = document.createElement('div');
+
+    formGroup2.appendChild(labelHelp);
+
+    const inputHelp = document.createElement('p');
+    inputHelp.textContent = 'If you provide an existing label the value will be replaced, otherwise it is skipped';
+
+    const inputHelp2 = document.createElement('p');
+    inputHelp2.textContent = 'If you provide a value of REMOVE, the label will be removed';
+
+    const inputHelp3 = document.createElement('p');
+    inputHelp3.textContent = 'spaces will be replaced with a -';
+
+    formGroup2.appendChild(inputHelp);
+    formGroup2.appendChild(inputHelp2);
+    formGroup2.appendChild(labelSelect);
+
+    const formGroup3 = document.createElement('div');
+    formGroup3.appendChild(labelInput);
+    formGroup3.appendChild(valueInput);
+    formGroup3.appendChild(addButton);
+
+    form.appendChild(formGroup2);
+    form.appendChild(formGroup3);
+
+    tempDiv.html(form)
+}
+
+
+function labelFormPopup() {
+    swal({
+        title: "Label Updater",
+        text: '<form id="temp-label-injector-form"></form>',
+        html: true,
+        showCancelButton: true,
+    }, function () {
+        addLabels()
+    })
+
+    labelForm()
+}
+
 function addLabels() {
-    var labels = prompt("Please enter the labels", "key1=value1,key2=value2");
-    if (labels != null) {
-        $.post("/plugins/docker.versions/server/AddLabels.php", { labels: labels }, function (data) {
-            location.reload();
+    const options = $('#label-injector-labels').find('option');
+    const labels = [];
+
+    options.each(function () {
+        const key = $(this).text().replace(" ", "-");
+        const value = $(this).val();
+        labels.push({ key: key, value: value });
+    });
+
+    const containers = $('#label-injector-containers').val().filter(ct => ct != "All")
+
+    if (labels.length > 0 && containers.length > 0) {
+        $('div.spinner.fixed').show();
+        $.post("/plugins/docker.labelInjector/server/AddLabels.php", { data: JSON.stringify({ labels, containers }) }, function (data) {
+            $('div.spinner.fixed').hide();
+            data = JSON.parse(data)
+
+            if (data.containers.length > 0) {
+                const containersString = data.containers.map(container => encodeURIComponent(container));
+                openDocker('update_container ' + containersString.join("*"), _('Updating following Containers: ' + containersString), '', 'loadlist');
+            }
         });
     }
 }
--- src/docker.labelInjector/usr/local/emhttp/plugins/docker.labelInjector/server/AddLabels.php
+++ src/docker.labelInjector/usr/local/emhttp/plugins/docker.labelInjector/server/AddLabels.php
@@ -5,50 +5,82 @@ $documentRoot = $_SERVER['DOCUMENT_ROOT'] ?? '/usr/local/emhttp';
 require_once ("$documentRoot/plugins/dynamix.docker.manager/include/DockerClient.php");
 require_once ("$documentRoot/webGui/include/Helpers.php");
 
-
-$dockerTemplates = new DockerTemplates();
 $dockerUpdate = new DockerUpdate();
-$templates = $dockerTemplates->getTemplates('user');
 
+$data = json_decode($_POST["data"]);
 
-foreach ($templates as $template) {
+$containerNames = $data->containers;
+$inputs = $data->labels;
+
+function getUserTemplateInsensitive($Container)
+{
+    $dockerTemplates = new DockerTemplates();
+
+    foreach ($dockerTemplates->getTemplates('user') as $file) {
+        $doc = new DOMDocument('1.0', 'utf-8');
+        $doc->load($file['path']);
+        $Name = strtolower($doc->getElementsByTagName('Name')->item(0)->nodeValue) ?? '';
+        if ($Name == $Container)
+            return $file['path'];
+    }
+    return false;
+}
+
+$updatedContainerNames = [];
+
+foreach ($containerNames as $containerName) {
+    $templatePath = getUserTemplateInsensitive($containerName);
     $changed = false;
 
-    $template_xml = simplexml_load_file($template["path"]);
+    $template_xml = simplexml_load_file($templatePath);
 
-    $inputs = ["asd" => "true"];
-    var_dump($template["path"]);
-    foreach ($inputs as $label => $value) {
-        $template_config_xml->xpath("//Config");
-        $template_label = $template_xml->xpath("//Config[@Type='Label'][@Target='$input']");
+    if ($template_xml) {
+        // echo "Actioning {$templatePath}\n";
+        $old_template_xml = $template_xml->asXML();
 
-        if ($template_label) {
-            if ($value == "REMOVE") {
-                // TODO: logic to remove label
-            } else if ($template_label->Value != $value) {
-                $template_label[0][0] = $value;
-                // $changed = true;
+        foreach ($inputs as $input) {
+            $label = $input->key;
+            $value = $input->value;
+
+            $template_label = $template_xml->xpath("//Config[@Type='Label'][@Target='$label']");
+            if ($template_label) {
+                if ($value == "REMOVE") {
+                    // echo "Removing $label\n";
+                    $dom = dom_import_simplexml($template_label[0]);
+                    $dom->parentNode->removeChild($dom);
+                    $changed = true;
+                } else if ($template_label[0][0] != $value) {
+                    // echo "Updating $label to $value\n";
+                    $template_label[0][0] = $value;
+                    $changed = true;
+                }
+            } else if ($value != "REMOVE") {
+                // echo "Adding $label with $value\n";
+                $newElement = $template_xml->addChild('Config');
+                $newElement->addAttribute('Name', $label);
+                $newElement->addAttribute('Target', $label);
+                $newElement->addAttribute('Default', "");
+                $newElement->addAttribute('Mode', "");
+                $newElement->addAttribute('Description', "");
+                $newElement->addAttribute('Type', 'Label');
+                $newElement->addAttribute('Display', 'always');
+                $newElement->addAttribute('Required', 'false');
+                $newElement->addAttribute('Mask', 'false');
+
+                $newElement[0] = $value;
+                $changed = true;
             }
-        } else {
-            $newElement = $config->addChild('Config');
-            $newElement->addAttribute('Type', 'Label');
-            $newElement->addAttribute('Target', $label);
-            $newElement->addAttribute('Value', $value);
-            $template_xml->asXML($template["path"]);
-            // $changed = true;
         }
-        var_dump($template_xml->xpath("//Config[@Type='Label'][@Target='$input']"));
-    }
 
-    if ($changed) {
-        // Format output and save to file if there were any commited changes
-        //$this->debug("Saving template modifications to '$file");
-        $dom = new DOMDocument('1.0');
-        $dom->preserveWhiteSpace = false;
-        $dom->formatOutput = true;
-        $dom->loadXML($template->asXML());
-        file_put_contents($file, $dom->saveXML());
+        if ($changed) {
+            // Backup Juust incase
+            file_put_contents($templatePath . "." . (new DateTime())->format('Y.m.d.H.I.s') . ".bak", $old_template_xml);
+            file_put_contents($templatePath, $template_xml->asXML());
+            $updatedContainerNames[] = $containerName;
+        }
     }
 }
 
+
+echo json_encode(["containers" => array_slice($updatedContainerNames, 0, 1)]);
 ?>

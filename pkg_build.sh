a6841f514c0db576e91c872a339a42d3d39dc011
phyzical <5182053+phyzical@users.noreply.github.com>
2024-10-03 15:43:56 +0800

    fix build ci

--- pkg_build.sh
+++ pkg_build.sh
@@ -18,18 +18,26 @@ rsync -av --progress src/$plugin_name/ "$tmpdir" --exclude .git --exclude tmp --
 cd "$tmpdir" || exit
 
 tar_command="tar"
+sed_prefix="-i"
+
 if [[ "$(uname)" == "Darwin" ]]; then
     tar_command="gtar"
+    sed_prefix="-i ''"
 fi
 $tar_command --owner=0 --group=0 -cJf "$filename" .
 
 cd - || exit
 
+if [[ "$(uname)" == "Darwin" ]]; then
+    md5hash=$(md5 -q "$filename")
+else
+    md5hash=$(md5sum "$filename" | awk '{ print $1 }')
+fi
+
 rm -rf "$tmpdir"
 
-sed -i '' 's/<!ENTITY version ".*">/<!ENTITY version "'"$version"'">/' $plugin_name.plg
-md5hash=$(md5 -q "$filename")
-sed -i '' 's/<!ENTITY md5 ".*">/<!ENTITY md5 "'"$md5hash"'">/' $plugin_name.plg
+sed "$sed_prefix" 's/<!ENTITY version ".*">/<!ENTITY version "'"$version"'">/' $plugin_name.plg
+sed "$sed_prefix" 's/<!ENTITY md5 ".*">/<!ENTITY md5 "'"$md5hash"'">/' $plugin_name.plg
 
 echo "MD5: $(md5sum "$filename")"
 echo "once pushed install via https://raw.githubusercontent.com/phyzical/$plugin_name/main/$plugin_name.plg"
